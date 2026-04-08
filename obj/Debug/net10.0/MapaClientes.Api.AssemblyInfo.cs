f07a7beac7505dbc2b3f942677336c899568028d
Matheus Gomes <aquersaber@gmail.com>
2026-04-07 22:16:17 -0300

    implementacao no program

--- obj/Debug/net10.0/MapaClientes.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/MapaClientes.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapaClientes.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd402ad30ebced369a78f571173f5be324efbab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53776d7fa2d8df1847dc5fb2bafe95e56323b8f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapaClientes.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapaClientes.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
